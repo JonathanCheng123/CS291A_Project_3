d4d38ad1cb2ec6031c82e844397a850cd1679742
JonathanCheng123 <jcmohr9@gmail.com>
2025-11-06 12:38:18 -0800

    initial implementation of health_controller.rb

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,2 +1,11 @@
 class HealthController < ApplicationController
+    # index should occur without authentication
+    skip_before_action :authenticate_user!, only:[:index]
+    
+    def index
+        render json: {
+        status: 'ok',
+        timestamp: Time.current.iso8601
+        }
+    end
 end
